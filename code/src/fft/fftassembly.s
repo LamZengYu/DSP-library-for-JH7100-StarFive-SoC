6e46664c8b07ab18e4a140ceb03df2bfcba3d402
LamZengYu <154478158+LamZengYu@users.noreply.github.com>
2024-06-09 00:27:45 +0800

    Update fftassembly.s

--- code/src/fft/fftassembly.s
+++ code/src/fft/fftassembly.s
@@ -83,8 +83,8 @@ fft:
 .L4:
 	lw	a5,-24(s0)
 	srliw	a5,a5,1
-	sw	a5,-24(s0)
 	lw	a4,-24(s0)
+	sw	a5,-24(s0)
 	lw	a5,-20(s0)
 	and	a5,a4,a5
 	sext.w	a5,a5
@@ -102,28 +102,28 @@ fft:
 	sext.w	a5,a5
 	bleu	a5,a4,.L7
 	lwu	a5,-20(s0)
-	slli	a5,a5,2
 	ld	a4,-104(s0)
+	slli	a5,a5,2
 	add	a5,a4,a5
 	flw	fa5,0(a5)
+	lwu	a5,-20(s0)
 	fsw	fa5,-80(s0)
-	lwu	a5,-20(s0)
 	slli	a5,a5,2
 	ld	a4,-112(s0)
 	add	a5,a4,a5
 	flw	fa5,0(a5)
+	lw	a5,-28(s0)
 	fsw	fa5,-84(s0)
-	lw	a5,-28(s0)
 	addiw	a5,a5,1
 	sext.w	a5,a5
 	slli	a5,a5,32
+	ld	a4,-104(s0)
 	srli	a5,a5,32
 	slli	a5,a5,2
-	ld	a4,-104(s0)
 	add	a4,a4,a5
 	lwu	a5,-20(s0)
-	slli	a5,a5,2
 	ld	a3,-104(s0)
+	slli	a5,a5,2
 	add	a5,a3,a5
 	flw	fa5,0(a4)
 	fsw	fa5,0(a5)
@@ -131,23 +131,23 @@ fft:
 	addiw	a5,a5,1
 	sext.w	a5,a5
 	slli	a5,a5,32
+	ld	a4,-112(s0)
 	srli	a5,a5,32
 	slli	a5,a5,2
-	ld	a4,-112(s0)
 	add	a4,a4,a5
 	lwu	a5,-20(s0)
-	slli	a5,a5,2
 	ld	a3,-112(s0)
-	add	a5,a3,a5
+	slli	a5,a5,2
 	flw	fa5,0(a4)
+	add	a5,a3,a5
 	fsw	fa5,0(a5)
 	lw	a5,-28(s0)
 	addiw	a5,a5,1
 	sext.w	a5,a5
 	slli	a5,a5,32
+	ld	a4,-104(s0)
 	srli	a5,a5,32
 	slli	a5,a5,2
-	ld	a4,-104(s0)
 	add	a5,a4,a5
 	flw	fa5,-80(s0)
 	fsw	fa5,0(a5)
@@ -155,9 +155,9 @@ fft:
 	addiw	a5,a5,1
 	sext.w	a5,a5
 	slli	a5,a5,32
+	ld	a4,-112(s0)
 	srli	a5,a5,32
 	slli	a5,a5,2
-	ld	a4,-112(s0)
 	add	a5,a4,a5
 	flw	fa5,-84(s0)
 	fsw	fa5,0(a5)
