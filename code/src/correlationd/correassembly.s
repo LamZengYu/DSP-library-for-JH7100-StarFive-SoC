7770c9bef1121222a72023821b4f5c17a801cd68
LamZengYu <154478158+LamZengYu@users.noreply.github.com>
2024-06-13 00:01:25 +0800

    Update correassembly.s

--- code/src/correlationd/correassembly.s
+++ code/src/correlationd/correassembly.s
@@ -79,7 +79,7 @@ correlation:
 	ld	a5,-64(s0)
 	addi	a5,a5,8
 	fld	fa5,0(a5)
-	fmadd.d	fa5,fa3,fa5,fa4
+	fmadd.d	fa4,fa3,fa5,fa4
 	lw	a5,-68(s0)
 	slli	a5,a5,3
 	addi	a5,a5,-8
@@ -151,7 +151,7 @@ correlation:
 	addi	a5,a5,-16
 	add	a5,a4,a5
 	fld	fa5,0(a5)
-	fmadd.d	fa5,fa3,fa5,fa4
+	fmadd.d	fa4,fa3,fa5,fa4
 	ld	a5,-56(s0)
 	addi	a5,a5,16
 	fld	fa3,0(a5)
