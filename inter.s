8c1702abc45ee6854d40b5a4ef89ac335f1152ed
LamZengYu <154478158+LamZengYu@users.noreply.github.com>
2024-06-20 00:16:46 +0800

    Create inter.s

---
+++ inter.s
@@ -0,0 +1,11 @@
+mv	a5, a2
+sw	a5, -116(s0)
+lw	a5, -116(s0)
+srliw	a4, a5, 31
+//intermediate operations that 
+will change the value of a5
+sw	a5, -20(s0)
+lw	a5, -116(s0)
+sw	a5, -24(s0)
+li	a5, 1
+sw	a5, -28(s0)
