8672e65f929f7871fcebe549410ddbafa0a2a045
LamZengYu <154478158+LamZengYu@users.noreply.github.com>
2024-06-20 05:33:50 +0800

    Update fft_opt_d.c

--- src/fft/fft_opt_d.c
+++ src/fft/fft_opt_d.c
@@ -22,8 +22,8 @@ void fft_opt_d(double data_re[], double data_im[], const int N)
         data_im[position] = temp_im;
     }
 
-    while(target & (mask >>=1))
-        target &= ~mask;
+    while(target & (mask >>=1)) {
+        target &= ~mask;}
         target |= mask;
         mask = N;
 
