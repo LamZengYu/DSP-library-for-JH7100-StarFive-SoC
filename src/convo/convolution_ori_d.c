2ddb240ccaad090e1dd478bb96e1aa56b400ea78
LamZengYu <154478158+LamZengYu@users.noreply.github.com>
2024-06-20 04:53:31 +0800

    Create convolution_ori_d.c

---
+++ src/convo/convolution_ori_d.c
@@ -0,0 +1,20 @@
+double* convolve(double h[], double x[], int lenH, int lenX, int* lenY)
+{
+  int nconv = lenH+lenX-1;
+  (*lenY) = nconv;
+  double *y = (double*) calloc(nconv, sizeof(double));
+
+  for (int i=0; i<nconv; i++)
+  {
+    int x_start = MAX(0,i-lenH+1); 
+    int h_start = MIN(i,lenH-1);
+    int x_end   = MIN(i+1,lenX);
+
+    for(int j=x_start; j<x_end; j++)
+    {
+      y[i] += h[h_start]*x[j];
+      h_start--;
+    }
+  }
+  return y;
+}
