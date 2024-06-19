ddc1a094e9ae702d05c549ae6d13b273e74cb39c
LamZengYu <154478158+LamZengYu@users.noreply.github.com>
2024-06-20 04:40:54 +0800

    Update and rename fft_ori to fft_ori_f.c

---
+++ src/fft/fft_ori_f.c
@@ -0,0 +1,57 @@
+void fft_ori_f(float data_re[], float data_im[], const int N)
+{
+  unsigned int target = 0;
+  float temp_re, temp_im;
+
+  //Bit reversal
+  for(unsigned int position=0; position<N; position++)
+  {
+    if(target>position) {
+      float temp_re = data_re[target];
+      float temp_im = data_im[target];
+      data_re[target] = data_re[position];
+      data_im[target] = data_im[position];
+      data_re[position] = temp_re;
+      data_im[position] = temp_im;
+    }
+    unsigned int mask = N;
+    while(target & (mask >>=1))
+      target &= ~mask;
+    
+      target |= mask;
+  }
+  
+  //Computation
+  unsigned int jump, match;
+  float step_d, twiddle_re, twiddle_im, product_re, product_im;
+
+  for(unsigned int step=1; step<N; step <<=1)
+  {
+    jump = step << 1;
+    step_d = (float) step;
+    twiddle_re = 1.0;
+    twiddle_im = 0.0;
+    for(unsigned int group=0; group<step; group++)
+    {
+      for(unsigned int pair=group; pair<N; pair+=jump)
+      {
+        int match = pair + step;
+        product_re = twiddle_re*data_re[match]-twiddle_im*data_im[match];
+        product_im = twiddle_im*data_re[match]+twiddle_re*data_im[match];
+        data_re[match] = data_re[pair]-product_re;
+        data_im[match] = data_im[pair]-product_im;
+        data_re[pair] += product_re;
+        data_im[pair] += product_im;
+      }
+      
+      if(group+1 == step)
+      {
+        continue;
+      }
+
+      float angle =  -M_PI*((float) group+1)/step_d;
+      twiddle_re = cos(angle);
+      twiddle_im = sin(angle);
+    }
+  }
+}
