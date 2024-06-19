c294df461f437d644abf3e1bae7254282e87a1e4
LamZengYu <154478158+LamZengYu@users.noreply.github.com>
2024-06-20 00:21:03 +0800

    Update inter.s

--- inter.s
+++ inter.s
@@ -2,7 +2,7 @@ mv	a5, a2
 sw	a5, -116(s0)
 srliw	a4, a5, 31
 sw	a5, -24(s0)
-/*intermediate operations that will change the value of a5
+/*Operations that change the value of a5
 sw	a5, -20(s0)
 li	a5, 1
 sw	a5, -28(s0)
