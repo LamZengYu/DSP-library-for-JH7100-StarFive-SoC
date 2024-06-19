d8a46dac6daff4c74e9124fb533a3315014d0efd
LamZengYu <154478158+LamZengYu@users.noreply.github.com>
2024-06-20 04:04:29 +0800

    Create jh7100_dsp.h

---
+++ include/jh7100_dsp.h
@@ -0,0 +1,23 @@
+void fft_ori_f(float data_re[], float data_im[],const int N);
+void fft_opt_f(float data_re[], float data_im[],const int N);
+void fft_asm_f(float data_re[], float data_im[],const int N);
+
+void fft_ori_d(double data_re[], double data_im[],const int N);
+void fft_opt_d(double data_re[], double data_im[],const int N);
+void fft_asm_d(double data_re[], double data_im[],const int N);
+
+float* correlation_ori_f(float h[], float x[], int lenH, int lenX, int* lenY);
+float* correlation_opt_f(float h[], float x[], int lenH, int lenX, int* lenY);
+float* correlation_asm_f(float h[], float x[], int lenH, int lenX, int* lenY);
+
+double* correlation_ori_d(double h[], double x[], int lenH, int lenX, int* lenY);
+double* correlation_opt_d(double h[], double x[], int lenH, int lenX, int* lenY);
+double* correlation_asm_d(double h[], double x[], int lenH, int lenX, int* lenY);
+
+float* convolution_ori_f(float h[], float x[], int lenH, int lenX, int* lenY);
+float* convolution_opt_f(float h[], float x[], int lenH, int lenX, int* lenY);
+float* convolution_asm_f(float h[], float x[], int lenH, int lenX, int* lenY);
+
+double* convolution_ori_d(double h[], double x[], int lenH, int lenX, int* lenY);
+double* convolution_opt_d(double h[], double x[], int lenH, int lenX, int* lenY);
+double* convolution_asm_d(double h[], double x[], int lenH, int lenX, int* lenY);
