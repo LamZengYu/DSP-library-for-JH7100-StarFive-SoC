6ff629180092359d5b0ba5ed519d94e5e6921962
LamZengYu <154478158+LamZengYu@users.noreply.github.com>
2024-06-20 05:28:31 +0800

    Update correlation_opt_d.c

--- src/corre/correlation_opt_d.c
+++ src/corre/correlation_opt_d.c
@@ -1,4 +1,8 @@
 #include "../../include/dsp.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
 
 double* correlation(double h[], double x[], int lenH, int lenX, int* lenY)
 {
